c6750c6a5e9b31c2f8c380caa7a27c3b03dbde62
LucunJi <1063464500@qq.com>
2020-09-27 21:31:01 -0500

    First commit

---
+++ src/main/java/io/github/lucunji/crispycrispysandshark/blocks/SandSharkBlock.java
@@ -0,0 +1,9 @@
+package io.github.lucunji.crispycrispysandshark.blocks;
+
+import net.minecraft.block.Block;
+
+public class SandSharkBlock extends Block {
+    public SandSharkBlock(Properties properties) {
+        super(properties);
+    }
+}
